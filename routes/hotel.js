6acfed9f6a227d0e92c2b671b42b3151b322272c
Likhitha Gopi <89407077+Likhitha-Gopi@users.noreply.github.com>
2021-11-28 21:59:44 -0600

    Added logins and validations to the page

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,8 +1,26 @@
 var express = require('express');
 const hotel_controlers= require('../controllers/hotels'); 
 var router = express.Router();
-
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => {
+    console.log(req) 
+    if (req.user){ 
+      return next(); 
+    }
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET home page. */
 router.get('/', hotel_controlers.hotel_view_all_Page);
+
+router.get('/detail', hotel_controlers.hotel_view_one_Page); 
+
+router.get('/create',secured, hotel_controlers.hotel_create_Page); 
+
+router.get('/update',secured,hotel_controlers.hotel_update_Page);
+
+router.get('/delete',secured, hotel_controlers.hotel_delete_Page);
 
 module.exports = router;
